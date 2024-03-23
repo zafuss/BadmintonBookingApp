e0db7550cdf8588cf13fbe19bef26e6374f9517e
Đặng Hoàng Gia Phú <zafus2103@gmail.com>
2024-03-23 14:57:46 +0700

    bug fixed completely

--- BadmintonBookingApp/Program.cs
+++ BadmintonBookingApp/Program.cs
@@ -1,4 +1,5 @@
 using BadmintonBookingApp.Data;
+using BadmintonBookingApp.Models.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,10 +11,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+//builder.Services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+options.SignIn.RequireConfirmedAccount = true)
+
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders()
+.AddDefaultUI();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
